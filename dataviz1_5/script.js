c3d0aa81405b34dfb3bcf671a2a999cf31da73ac
fdenunez <federicodenunez@gmail.com>
2023-04-19 11:28:25 -0300

    RUIDOOOOO

--- dataviz1_5/script.js
+++ dataviz1_5/script.js
@@ -7,10 +7,10 @@ d3.dsv(';', 'trafico.csv', d3.autoType).then(data => {
 
   for (let index = 0; index < data.length; index++) { // itera todas las filas
     const element = data[index]["fecha_ingreso"];
-    if (element != undefined) { // check if fecha_ingreso property exists
-      let day = parseInt(element.split('/')[0], 10); // get day from date string
-      let mes = parseInt(element.split('/')[1], 10) - 1; // get month from date string and subtract 1 to get index
-      if (day >= 15) { // check if day is greater or equal to 15
+    if (element != undefined) { // checkea si el valor no es vacío
+      let day = parseInt(element.split('/')[0], 10); // extrae el día
+      let mes = parseInt(element.split('/')[1], 10) - 1; // 
+      if (day >= 15) { // se fija s
         counts[mes]++;
       }
     }
