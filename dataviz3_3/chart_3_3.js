8b47febd2df0e0283b643834174f2fdcf66c6a66
luknda <lucasadnac@hotmail.com>
2023-04-25 10:35:12 -0300

    xd

---
+++ dataviz3_3/chart_3_3.js
@@ -0,0 +1,17 @@
+d3.dsv(';', 'c.csv', d3.autoType).then(data => {
+
+  let chart = Plot.plot({
+    marks: [
+      Plot.barY(data, {
+        x: "domicilio_barrio",
+        y: d.properties.DENUNCIAS,
+        //fill: 'country',
+        //sort: 'pop',
+        //title: d => d.country + '\n' + d.pop,
+      }),
+    ],
+    marginLeft: 70,
+    width: 300,
+  })
+  d3.select('#chart').append(() => chart)
+})
